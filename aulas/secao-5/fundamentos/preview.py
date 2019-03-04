aa9de66c0b5baa6ec5f62b3366667573588c66a6
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-04 20:24:35 -0300

    [S5A37] Operadores de membro e operadores de identidade.

--- aulas/secao-5/fundamentos/preview.py
+++ aulas/secao-5/fundamentos/preview.py
@@ -1,4 +1,17 @@
+'''
 lista = [1, 2, 3]
 dicionario = {'nome': 'Pedro', 'idade': 22}
 
 a = 10
+'''
+
+x = 3
+y = x
+z = 3
+
+lista_a = [1, 2, 3]
+lista_b = lista_a
+lista_c = [1, 2, 3]
+
+# lista_c[1] = 200
+# lista_a[1] = 300

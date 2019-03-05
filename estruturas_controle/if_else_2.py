1f09c705f4c3398840b3fae6e3049886ed9d5840
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-05 15:27:00 -0300

    [S7A74] Segundo exemplo de estruturas de controle, utilizando if, elif, else e tupla.

---
+++ estruturas_controle/if_else_2.py
@@ -0,0 +1,17 @@
+def faixa_etaria(idade):
+    if 0 <= idade < 18:
+        return 'Menor de idade'
+    elif idade in range(18, 65):
+        return 'Adulto'
+    elif idade in range(65, 100):
+        return 'Melhor idade'
+    elif idade >= 100:
+        return 'Centenário'
+    else:
+        return 'Idade inválida'
+
+
+if __name__ == '__main__':
+    idades = (17, 0, 35, 87, 113, -2, 18, 64, 65, 100)
+    for idade in idades:
+        print(f'{idade}: {faixa_etaria(idade)}')

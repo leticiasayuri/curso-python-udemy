43353238eb253c2e76ba8400de523028e740407b
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-05 13:51:19 -0300

    [S6A68] Extração do código que printa uma mensagem de ajuda ao usuário para uma função help().

--- fundamentos_projetos/area_circulo.py
+++ fundamentos_projetos/area_circulo.py
@@ -7,10 +7,14 @@ def circulo(raio):
     return pi * float(raio) ** 2
 
 
+def help():
+    print("É necessário informador o raio do círculo.")
+    print("Sintaxe: {} <raio>".format(sys.argv[0][2:]))
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("É necessário informador o raio do círculo.")
-        print("Sintaxe: {} <raio>".format(sys.argv[0][2:]))
+        help()
     else:
         raio = sys.argv[1]
         area = circulo(raio)

a363ab3d542cd6ef06be6cdc0687e6ff5ae1f3d4
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-05 14:43:05 -0300

    [S6A74] Criação de classes com cores para alterar cor em mensagens de ero.

--- fundamentos_projetos/area_circulo.py
+++ fundamentos_projetos/area_circulo.py
@@ -4,6 +4,11 @@ import sys
 import errno
 
 
+class TerminalColor:
+    ERRO = '\033[91m'
+    NORMAL = '\033[0m'
+
+
 def circulo(raio):
     return pi * float(raio) ** 2
 
@@ -19,7 +24,8 @@ if __name__ == '__main__':
         sys.exit(errno.EPERM)
     if not sys.argv[1].isnumeric():
         help()
-        print('O raio deve ser um valor numérico')
+        print(TerminalColor.ERRO +
+              'O raio deve ser um valor numérico' + TerminalColor.NORMAL)
         sys.exit(errno.EINVAL)
     raio = sys.argv[1]
     area = circulo(raio)

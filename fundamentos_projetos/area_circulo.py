c9708bdc82017914a37569e99f5e6ed488b3e487
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-05 10:54:07 -0300

    [S6A64] Cálculo do valor da área do círculo na fuñção circulo e retorno do valor para que este possa ser usado em outros pontos do código.

--- fundamentos_projetos/area_circulo.py
+++ fundamentos_projetos/area_circulo.py
@@ -3,9 +3,10 @@ from math import pi
 
 
 def circulo(raio):
-    print('Área do círculo ', pi * float(raio) ** 2)
+    return pi * float(raio) ** 2
 
 
 if __name__ == '__main__':
     raio = input('Informe o raio: ')
-    circulo(raio)
+    area = circulo(raio)
+    print('Área do circulo: ', area)

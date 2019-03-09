1321d2acc54dba8444745eff0a95abd14da41e63
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-09 17:49:10 -0300

    [S9A100] Remoção das quebras de linha entre os registros printados utilizando a função strip().

--- manipulacao_arquivos/io_stream.py
+++ manipulacao_arquivos/io_stream.py
@@ -1,6 +1,6 @@
 arquivo = open('pessoas.csv')
 
 for registro in arquivo:
-    print('Nome: {}, Idade: {}'.format(*registro.split(',')))
+    print('Nome: {}, Idade: {}'.format(*registro.strip().split(',')))
 
 arquivo.close()

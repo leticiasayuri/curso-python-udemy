27afaeed46ac0300132a3d3bd54f8702c37d6bfc
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 11:26:51 -0300

    [S11A121] Callable com packing e unpacking, exemplificando o uso de parâmetros opcionais.

---
+++ funcoes/callable_packing_unpacking.py
@@ -0,0 +1,17 @@
+def calcula_preco_final(preco_bruto, calcula_imposto, *params):
+    return preco_bruto + preco_bruto * calcula_imposto(*params)
+
+
+def imposto_x(importado):
+    return 0.22 if importado else 0.13
+
+
+def imposto_y(explosivo, fator_multiplicacao=1):
+    return 0.11 * fator_multiplicacao if explosivo else 0
+
+
+if __name__ == '__main__':
+    preco_bruto = 134.98
+    preco_final = calcula_preco_final(preco_bruto, imposto_x, True)
+    preco_final = calcula_preco_final(preco_final, imposto_y, True, 1.5)
+    print(f'Preço final R$ {preco_final}')

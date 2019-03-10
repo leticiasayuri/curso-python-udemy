841c595dc36c3f648d7e1df7759abdc9f6003324
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 20:02:14 -0300

    [S12A135] Import direto de funções provenientes de módulos.

--- pacotes/pacote.py
+++ pacotes/pacote.py
@@ -1,11 +1,5 @@
-from pacote1 import modulo1
-from pacote1 import modulo2
-from pacote2 import modulo1 as modulo1_subtracao
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import subtracao
 
-print(type(modulo1))
-print(modulo1.soma(2, 3))
-
-modulo2.main()
-
-print('Soma ', modulo1.soma(3, 2))
-print('Subtração ', modulo1_subtracao.subtracao(3, 2))
+print('Soma', soma(3, 2))
+print('Subtracao', subtracao(3, 2))

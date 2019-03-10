04fb7a696ee496848b58398178a4e328d083c118
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-09 21:42:39 -0300

    [S10A108] Utilização do comando completo de list comprehension para criar uma lista com condicional.

--- list_comprehension/comprehension.py
+++ list_comprehension/comprehension.py
@@ -1,9 +1,24 @@
 # [ expressão for item in list ]
+print('List Comprehension sem condição')
 dobros = [i * 2 for i in range(10)]
 print(dobros)
 
 # Versão "normal"
+print('\nVersão "normal"')
 dobros = []
 for i in range(10):
     dobros.append(i * 2)
 print(dobros)
+
+# [ expressão for item in list if condicional ]
+print('\nList Comprehension com condição')
+dobros_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
+print(dobros_dos_pares)
+
+# Versão "normal"
+print('\nVersão "normal"')
+dobros_dos_pares = []
+for i in range(10):
+    if i % 2 == 0:
+        dobros_dos_pares.append(i * 2)
+print(dobros_dos_pares)

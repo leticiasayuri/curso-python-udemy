bc75448abe6a94ebf3e262559b950f389723c649
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 19:58:36 -0300

    [S12A134] Import de dois módulos com o mesmo nome, diferenciando-os com um alias.

--- pacotes/pacote.py
+++ pacotes/pacote.py
@@ -1,7 +1,11 @@
 from pacote1 import modulo1
 from pacote1 import modulo2
+from pacote2 import modulo1 as modulo1_subtracao
 
 print(type(modulo1))
 print(modulo1.soma(2, 3))
 
 modulo2.main()
+
+print('Soma ', modulo1.soma(3, 2))
+print('Subtração ', modulo1_subtracao.subtracao(3, 2))

1af1d8296819fc42382bf288fc382951dfa809e4
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 10:08:20 -0300

    [S11A117] Função para criação de lista html utilizando packing de parâmetros.

--- funcoes/gerador_html.py
+++ funcoes/gerador_html.py
@@ -1,6 +1,11 @@
-def tag_bloco(texto, classe='success', inline=False):
+def tag_bloco(conteudo, classe='success', inline=False):
     tag = 'span' if inline else 'div'
-    return f'<{tag} class="{classe}">{texto}</{tag}>'
+    return f'<{tag} class="{classe}">{conteudo}</{tag}>'
+
+
+def tag_lista(*itens):
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
+    return f'<ul>{lista}</ul>'
 
 
 if __name__ == '__main__':
@@ -13,5 +18,7 @@ if __name__ == '__main__':
     print(tag_bloco('bloco'))
     print(tag_bloco('inline e classe', 'info', True))
     print(tag_bloco('inline', inline=True))
-    print(tag_bloco(inline=True, texto='inline'))
+    print(tag_bloco(inline=True, conteudo='inline'))
     print(tag_bloco('falhou', classe='error'))
+
+    print(tag_bloco(tag_lista('Item 2', 'Item 2'), classe='info'))

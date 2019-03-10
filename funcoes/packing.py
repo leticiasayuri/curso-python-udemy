eea0723e65ddcc579785a212b5af9e49f890cacd
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 09:51:03 -0300

    [S11A116] Exemplo de Unpacking, em que uma tupla ou lista é desempacotada em argumentos para serem passados à uma função.

--- funcoes/packing.py
+++ funcoes/packing.py
@@ -16,6 +16,17 @@ def soma_n(*numeros):
 if __name__ == '__main__':
     print(soma_dois(2, 3))
     print(soma_tres(2, 4, 6))
+
+    # Packing
+    # Empacotamento dos argumentos em uma tupla para serem passados a função
     print(soma_n(1))
     print(soma_n(1, 1))
     print(soma_n(1, 1, 1, 1, 1, 1, 1))
+
+    # Unpacking
+    # Desempacotamento de uma tupla/lista para os argumentos serem passados a função
+    tupla_nums = (1, 2, 3)
+    print(soma_tres(*tupla_nums))
+
+    lista_nums = [1, 2, 3]
+    print(soma_tres(*lista_nums))

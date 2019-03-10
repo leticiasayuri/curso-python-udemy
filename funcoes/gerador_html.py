4d79fb91dc7802acf04443f8bb400e1407a58909
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-10 10:40:20 -0300

    [S11A119] Uso de Callable na função de geração de HTML.

--- funcoes/gerador_html.py
+++ funcoes/gerador_html.py
@@ -1,6 +1,7 @@
-def tag_bloco(conteudo, classe='success', inline=False):
+def tag_bloco(conteudo, *args, classe='success', inline=False):
     tag = 'span' if inline else 'div'
-    return f'<{tag} class="{classe}">{conteudo}</{tag}>'
+    html = conteudo if not callable(conteudo) else conteudo(*args)
+    return f'<{tag} class="{classe}">{html}</{tag}>'
 
 
 def tag_lista(*itens):
@@ -10,15 +11,17 @@ def tag_lista(*itens):
 
 if __name__ == '__main__':
     # Testes (assertions)
-    assert tag_bloco('Incluído com sucesso!') == \
-        '<div class="success">Incluído com sucesso!</div>'
-    assert tag_bloco('Impossível excluir!', 'error') == \
-        '<div class="error">Impossível excluir!</div>'
+    # assert tag_bloco('Incluído com sucesso!') == \
+    #    '<div class="success">Incluído com sucesso!</div>'
+    # assert tag_bloco('Impossível excluir!', 'error') == \
+    #    '<div class="error">Impossível excluir!</div>'
 
     print(tag_bloco('bloco'))
-    print(tag_bloco('inline e classe', 'info', True))
+    print(tag_bloco('inline e classe', classe='info', inline=True))
     print(tag_bloco('inline', inline=True))
     print(tag_bloco(inline=True, conteudo='inline'))
     print(tag_bloco('falhou', classe='error'))
 
     print(tag_bloco(tag_lista('Item 2', 'Item 2'), classe='info'))
+
+    print(tag_bloco(tag_lista, 'Sábado', 'Domingo', classe='info', inline=True))

e2370a2db6c48f29f739d0dc2fa9beb487011d0a
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-09 22:47:47 -0300

    [S10A110] Percorrimento de um Generator com for.

--- list_comprehension/comprehension.py
+++ list_comprehension/comprehension.py
@@ -25,7 +25,10 @@ print(dobros_dos_pares)
 
 print('\nGenerator')
 generator = (i ** 2 for i in range(10) if i % 2 == 0)
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
+# print(next(generator))
+# print(next(generator))
+# print(next(generator))
+# print(next(generator))
+
+for numero in generator:
+    print(numero)

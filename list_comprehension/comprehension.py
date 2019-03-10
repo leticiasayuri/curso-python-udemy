b26ce3b12a6711fb555593a6eb784a5cb6129c2b
Leticia Sayuri <leticiasyrk@gmail.com>
2019-03-09 21:51:39 -0300

    [S10A109] Geração de lista com generator, que consome menos memória, pois gera os elementos sob demanda.

--- list_comprehension/comprehension.py
+++ list_comprehension/comprehension.py
@@ -22,3 +22,10 @@ for i in range(10):
     if i % 2 == 0:
         dobros_dos_pares.append(i * 2)
 print(dobros_dos_pares)
+
+print('\nGenerator')
+generator = (i ** 2 for i in range(10) if i % 2 == 0)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))

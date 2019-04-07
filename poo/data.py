3cfa280c83ae2a8ca0f3f778181f524eba94cbc3
Leticia Sayuri <leticiasyrk@gmail.com>
2019-04-07 18:20:08 -0300

    [S13A143] Construtor personalizado com __init__().

--- poo/data.py
+++ poo/data.py
@@ -1,16 +1,15 @@
 class Data:
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
     def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
-data1 = Data()
-data1.dia = 5
-data1.mes = 12
-data1.ano = 2019
+data1 = Data(5, 12, 2019)
 print(data1)
 
-data2 = Data()
-data2.dia = 7
-data2.mes = 11
-data2.ano = 2020
+data2 = Data(7, 11, 2020)
 print(data2)

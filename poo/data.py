7da14bc455d0403c41b8929b7fb39f941a329e43
Leticia Sayuri <leticiasyrk@gmail.com>
2019-04-07 18:12:12 -0300

    [S13A142] Substituição do nome do método de to_str para __str__, que é um método suportado por todos os objetos Python, que tira a necessidade de chamar o método explicitamente, ou seja, de forma implícita, o Python entende que está sendo chamado o método __str__.

--- poo/data.py
+++ poo/data.py
@@ -1,5 +1,5 @@
 class Data:
-    def to_str(self):
+    def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
@@ -7,10 +7,10 @@ data1 = Data()
 data1.dia = 5
 data1.mes = 12
 data1.ano = 2019
-print(data1.to_str())
+print(data1)
 
 data2 = Data()
 data2.dia = 7
 data2.mes = 11
 data2.ano = 2020
-print(data2.to_str())
+print(data2)

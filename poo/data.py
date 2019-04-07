d4b5ab1814b3b2d36bf5b499ec29e8a21d02bb36
Leticia Sayuri <leticiasyrk@gmail.com>
2019-04-07 18:10:28 -0300

    [S13A142] Método to_str() que printa a data formatada.

--- poo/data.py
+++ poo/data.py
@@ -1,9 +1,16 @@
 class Data:
-    pass
+    def to_str(self):
+        return f'{self.dia}/{self.mes}/{self.ano}'
 
 
 data1 = Data()
 data1.dia = 5
 data1.mes = 12
 data1.ano = 2019
-print(f'{data1.dia}/{data1.mes}/{data1.ano}')
+print(data1.to_str())
+
+data2 = Data()
+data2.dia = 7
+data2.mes = 11
+data2.ano = 2020
+print(data2.to_str())

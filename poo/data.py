26d51f477973e8a2bf8c05c651445b1ecb8e5d76
Leticia Sayuri <leticiasyrk@gmail.com>
2019-04-07 18:23:15 -0300

    [S13A143] Uso de parâmetros nomeados e opcionais (com valor padrão) para personalizar o construtor, visto que Python não permite mais de um construtor.

--- poo/data.py
+++ poo/data.py
@@ -1,5 +1,5 @@
 class Data:
-    def __init__(self, dia, mes, ano):
+    def __init__(self, dia=1, mes=1, ano=1970):
         self.dia = dia
         self.mes = mes
         self.ano = ano
@@ -13,3 +13,9 @@ print(data1)
 
 data2 = Data(7, 11, 2020)
 print(data2)
+
+data3 = Data()
+print(data3)
+
+data4 = Data(ano=2022)
+print(data4)
